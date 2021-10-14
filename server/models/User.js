84b9c0f8051633d29495c19b6533f503976f9ea1
saurabhhere <saurabhguptajpr@yahoo.in>
2021-10-14 23:17:51 +0530

    Add user model

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String, 
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+}, {timestamps: true})
+
+const Users = mongoose.model('users', userSchema);
+
+module.exports = Users;
